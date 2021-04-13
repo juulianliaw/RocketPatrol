9a2ff3025ee265f103fb6e6d21f7ff53e4a889f1
juulianliaw <jcliaw@ucsc.edu>
2021-04-13 00:17:17 -0700

    Spacehips and rocket reset upon collision

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -7,27 +7,6 @@ class Rocket extends Phaser.GameObjects.Sprite {
         this.isFiring = false;
         this.moveSpeed = 2;
     }
-
-    /*update() {
-        if(this.isFiring){
-            this.y -= this.movementSpeed;
-                if(this.y < borderUISize*3){
-                    this.y = game.config.height-borderUISize-borderPadding;
-                    this.isFiring = false;
-                }
-        }else{
-            if(keyLEFT.isDown) {
-                this.x -= this.movementSpeed;
-            }
-            if(keyRIGHT.isDown) {
-                this.x += this.movementSpeed;
-            }
-            if(Phaser.Input.Keyboard.JustDown(keyF)){
-                this.isFiring = true;
-            }
-        this.x = Phaser.Math.Clamp(this.x, borderUISize + borderPadding, game.config.width-borderUISize-borderPadding);
-        }
-    }*/
     update(){
         if(!this.isFiring){
             if(keyLEFT.isDown && this.x >= borderUISize + this.width){
@@ -46,6 +25,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.isFiring = false;
             this.y = game.config.height - borderUISize - borderPadding;
         }
+    }
+    reset(){
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
     
 } 
--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -11,7 +11,10 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         this.x -= this.moveSpeed;
         //wrap around from left to right
         if(this.x <= 0 - this.width){
-            this.x = game.config.width;
+            this.reset();
         }
+    }
+    reset(){
+        this.x = game.config.width;
     }
 }
